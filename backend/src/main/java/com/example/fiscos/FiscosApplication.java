ee670ccab8e65f8dbe4a15e25edcee6d75e2d34e
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-06-26 21:13:41 -0300

    feat: init backend

---
+++ backend/src/main/java/com/example/fiscos/FiscosApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/fiscos/FiscosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiscos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
