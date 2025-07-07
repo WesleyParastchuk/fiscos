01226d160628972873924c54c2ca5839d9a5052d
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-07-07 19:25:07 -0300

    feat: productInvoice entity

---
+++ backend/src/main/java/com/example/fiscos/model/ProductInvoice.java
@@ -0,0 +1,36 @@
+package com.example.fiscos.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "produtos_nota_fiscal")
+public class ProductInvoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "valor", nullable = false)
+    private BigDecimal value;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "nota_fiscal_id", nullable = false)
+    private Invoice invoice;
+}
