4b711cb6147a54f1c3c76e19523f36962b127119
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-07-07 19:24:16 -0300

    feat: product entity

---
+++ backend/src/main/java/com/example/fiscos/model/Product.java
@@ -0,0 +1,38 @@
+package com.example.fiscos.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "produtos")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @Column(name = "unidade_medida", nullable = false, unique = true)
+    private String unitOfMeasure;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductInvoice> productInvoices;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductClassification> productClassifications;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<UserProductClassification> userClassifications;
+
+}
