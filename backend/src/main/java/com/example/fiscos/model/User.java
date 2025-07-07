736d9d045d2df188ddba9046fd97f1e66bf1591c
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-07-07 19:23:15 -0300

    feat: user entity

---
+++ backend/src/main/java/com/example/fiscos/model/User.java
@@ -0,0 +1,35 @@
+package com.example.fiscos.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Invoice> invoices;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Classification> customClassifications;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<UserProductClassification> productClassifications;
+
+}
