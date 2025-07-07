c356dd4973b35e1fc57d91b77cb5c801c7b1d912
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-07-07 19:25:28 -0300

    feat: productClassification entity

---
+++ backend/src/main/java/com/example/fiscos/model/ProductClassification.java
@@ -0,0 +1,28 @@
+package com.example.fiscos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+@Table(name = "produtos")
+public class ProductClassification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "classificacao_id", nullable = false)
+    private Classification classification;
+
+}
