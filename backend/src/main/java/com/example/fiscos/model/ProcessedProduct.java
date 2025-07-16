804e44406a7944207864e40377a93b20c0509343
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-07-16 14:40:28 -0300

    feat: relacao para manter muitos produtos brutos apontando para o mesmo produto limpo

--- backend/src/main/java/com/example/fiscos/model/ProcessedProduct.java
+++ backend/src/main/java/com/example/fiscos/model/ProcessedProduct.java
@@ -1,19 +1,11 @@
 package com.example.fiscos.model;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.GenerationType;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -41,4 +33,6 @@ public class ProcessedProduct {
     @OneToMany(mappedBy = "processedProduct", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<ProductTagUser> productTagAssociations;
 
+    @OneToMany(mappedBy = "processedProduct", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<RawProduct> rawProducts;
 }
--- backend/src/main/java/com/example/fiscos/model/RawProduct.java
+++ backend/src/main/java/com/example/fiscos/model/RawProduct.java
@@ -1,19 +1,11 @@
 package com.example.fiscos.model;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.GenerationType;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -29,10 +21,13 @@ public class RawProduct {
     @Column(name = "nome", nullable = false)
     private String name;
 
-    @Column(name = "codigo", nullable = true)
+    @Column(name = "codigo")
     private String code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produto_id", nullable = false)
+    private ProcessedProduct processedProduct;
 
     @OneToMany(mappedBy = "rawProduct", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<ProductInvoice> productInvoices;
-
 }
